f423b23703291afcf23481f69578e43e1e9a2b09
Aleksandr Koltsin <58472735+sass207@users.noreply.github.com>
2025-04-24 22:45:29 +0300

    Update Protseduur.sql

--- Protseduur.sql
+++ Protseduur.sql
@@ -1,52 +1,47 @@
-CREATE DATABASE gutsuljak;
-use gutsuljak;
+CREATE DATABASE AleksandrTITpv23;
 
--- procduur 1
+USE AleksandrTITpv23;
+
 CREATE TABLE linnad(
-	linnID int PRIMARY KEY identity (1,1),
-	linnNimi varchar(30) unique,
-	elanikeArv int not null,
-	maakond varchar(25)
-	);
+linnID int PRIMARY KEY identity(1,1),
+linnNimi varchar(30) unique,
+elanikeArv int not null,
+maakond varchar(25)
+);
 
-	--Protseduur, mis lisab (INSERT) tabelisse andmed &
-	-- kohe näitab (SELECT) tabeli
+--Protseduur, mis lisab (INSERT)tabelisse andmed ja kohe näitab (SELECT) tabeli
 CREATE PROCEDURE lisaLinn
-	@linnNimi varchar(30),
-	@elanikeArv int,
-	@maakond varchar(25)
+@linnNimi varchar(30),
+@elanikeArv int,
+@maakond varchar(25)
 AS
 BEGIN
-	INSERT INTO linnad(linnNimi,elanikeArv,maakond)
-	VALUES (@linnNimi, @elanikeArv, @maakond);
-	SELECT * FROM linnad;
-END
+INSERT INTO linnad(linnNimi, elanikeArv, maakond)
+VALUES (@linnNimi, @elanikeArv, @maakond);
+SELECT * FROM linnad;
+END;
 
--- kutse
-Exec lisaLinn 'Tallinn', 650000, 'Harju';
-Exec lisaLinn 'Tartu', 20000, 'Tartu';
-Exec lisaLinn 'Pärnu', 30000, 'Pärnu';
-Exec lisaLinn 'Keila', 1000, 'Harju';
+--kutse
+EXEC lisaLinn 'Keila', 1000, 'Keila';
 
--- protseduuri kustutamine
+--protseduuri kustutamine
 DROP Procedure lisaLinn;
 
--- prtotseduur, mis kustutab tabelist linnID järgi
+--protseduur, mis kustutab tabelist linnID järgi
 CREATE PROCEDURE kustutaLinn
 @id int
 AS
 BEGIN
 SELECT * FROM linnad;
-DELETE FROM linnad where linnID=@id;
+DELETE FROM linnad WHERE linnId=@id;
 SELECT * FROM linnad;
 END
 
--- kutse 
+--kutse
 EXEC kustutaLinn 4;
-EXEC kustutaLinn @id=4;
 
--- protseduur mis uuendab tabeli ja suurendab elanike arv 10%
-CREATE Procedure uuenaLinn
+--protseduur mis uuendab tabeli ja suurendab elanike arv 10%
+CREATE PROCEDURE uuendaLinn
 @arv decimal(5,2)
 AS
 BEGIN
@@ -56,66 +51,59 @@ SELECT * FROM linnad;
 END
 
 --kutse
-EXEC uuenaLinn 0.05;
-DROP Procedure uuenaLinn;
-UPDATE linnad SET elanikeArv=20000 WHERE linnID=2;
+EXEC uuendaLinn 0.05;
+DROP PROCEDURE uuendaLinn;
+UPDATE linnad SET elanikeArv=200000 WHERE linnID=2;
 SELECT * FROM linnad;
 
--- protseedur " 'search film' 1 täht
-@taht CHAR 1
+CREATE PROCEDURE keskmineArv
+AS
 BEGIN
-	SELECT filmNimetus, pikkus
-    FROM film
-    WHERE filmNimetus LIKE CONCAT(taht, '%');
-END
---////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- muudatus add/drop column
---ADD column
-	--tabelinimi VARCHAR 25, veerunimi VARCHAR 25, tyyp VARCHAR 25
-BEGIN
+SELECT AVG(elanikeArv) AS 'Keskmine elanikeArv'
+FROM linnad;
 
-set @sqltegevus=concat('ALTER TABLE ', tabelinimi, ' ADD COLUMN ', veerunimi, ' ', tyyp);
+SELECT CAST(AVG(elanikeArv) AS decimal(7,2)) AS 'Keskmine elanikeArv' FROM linnad;
+END
+
+EXEC keskmineArv;
+
+--ainult 1 taht
+BEGIN
+SELECT filmNimetus, pikkus
+FROM film
+WHERE filmNimetus LIKE CONCAT(taht, '%');
+END
+
+--ADD
+BEGIN
+set @sqltegevus=concat('ALTER TABLE ', 
+tabelinimi, ' ADD COLUMN ' , veerunimi, ' ', tyyp);
 PREPARE STMT FROM @sqltegevus;
 EXECUTE STMT;
-
 END
---DROP column
-	--tabelinimi VARCHAR 25, veerunimi VARCHAR 25
-BEGIN
 
-set @sqltegevus=concat('ALTER TABLE ', tabelinimi, ' DROP COLUMN ', veerunimi);
+--DROP
+BEGIN
+set @sqltegevus=concat('ALTER TABLE ', 
+tabelinimi, ' DROP COLUMN ' , veerunimi);
 PREPARE STMT FROM @sqltegevus;
 EXECUTE STMT;
+END
 
-END;
 
-
---/////////////////////////////////////////////////////////////////////////////////////////////
--- pikkus
-	--pikkus_val INT
+--GET
 BEGIN
 DECLARE today TIMESTAMP DEFAULT CURRENT_DATE;
-	SELECT today, filmNimetus, pikkus
+    SELECT today, filmnimetus, pikkus
     FROM film
     WHERE pikkus < pikkus_val;
 END
 
--- FilmType search
--- 2d film... get_film_by_type
-	-- type VARCHAR 3
+
+--GET BY TYPE
 BEGIN
 SELECT f.filmNimetus, t.filmtype
 FROM film f
 INNER JOIN filmtype t ON f.filmtypeID=t.filmTypeID
-AND t.filmType= type;
-END
-
--- UPDATE RESIISOR
--- Dont work :(
-BEGIN
-    SELECT * FROM rezisoor;
-    UPDATE rezisoor
-    SET name = eesnimi, perenimi = perenimi
-    WHERE rezisoorID = @rezisoorID;
-    SELECT * FROM rezisoor;
+AND t.filmtype= type;
 END
